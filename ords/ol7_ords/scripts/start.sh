6f2398be2dcfcd9429449b9ee306f88743218280
hal9ccc <hal9ccc@googlemail.com>
2023-03-07 23:52:09 +0100

    fixed start.sh syntax error 9

--- ords/ol7_ords/scripts/start.sh
+++ ords/ol7_ords/scripts/start.sh
@@ -76,7 +76,7 @@ EOF
     echo "...not exact version"
   fi
 
-  if [[ "${RETVAL}" > "$AMV" ]]; then
+  if [[ "$RETVAL" > "$AMV" ]]; then
     if [[ "${RETVAL}" == *"$VALID"* ]]; then
       APEX_OK=1
       echo "...OK"
@@ -224,6 +224,7 @@ if [ "${FIRST_RUN}" == "true" ]; then
        --password-stdin <<EOF
 ${SYSDBA_PASSWORD}
 ${APEX_LISTENER_PASSWORD}
+${APEX_PUBLIC_USER_PASSWORD}
 EOF
 
   cp ords.war ${CATALINA_BASE}/webapps/
